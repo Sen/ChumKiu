b7cfa74aaed8aacc1c09766d875a346a1713a101
Sen <sen9ob@gmail.com>
2013-09-30 15:10:02 +0800

    init

---
+++ Gemfile
@@ -0,0 +1,5 @@
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'redis'
+
+gem 'minitest'
---
+++ app.rb
@@ -0,0 +1,65 @@
+require 'sinatra/base'
+require "sinatra/reloader"
+require "sinatra/json"
+require 'redis'
+
+class Armoire < Sinatra::Base
+  configure :development do
+    $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+    Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") do |lib| 
+      require File.basename(lib, '.*') 
+    end
+
+    register Sinatra::Reloader
+  end
+
+  helpers do
+    def redis
+      $redis
+    end
+
+    def set_redis(redis)
+      $redis = redis
+    end
+
+    def protected!
+      return if authorized?
+      halt 401, "Not authorized\n"
+    end
+
+    def authorized?
+      redis && redis.ping == 'PONG'
+    end
+  end
+
+  post '/api/connect' do
+    begin
+      if params[:sock]
+        set_redis Redis.new(path: params[:sock])
+      else
+        set_redis Redis.new(host: params[:host] || 'localhost',
+                            port: params[:port] || '6379')
+      end
+
+      json redis.info
+    rescue Exception => e
+      status 401
+      json error: e
+    end
+  end
+
+  get '/api/redis' do
+    begin
+      protected!
+      json redis.info
+    rescue Exception => e
+      status 401
+      json error: e
+    end
+  end
+
+  get '*' do
+    File.read(File.join('public', 'index.html'))
+  end
+
+end
